8b1c0aa35a70b334636b65e733cea3b638a5cf51
Odysseas Neslechanidis <odnes@tiptheiceberg.com>
2021-10-06 17:32:42 +0300

    Autosubstitute github ARTICLES_REPO urls with paths to raw txts.

--- jackhammer/config.py
+++ jackhammer/config.py
@@ -1,10 +1,25 @@
 from os import environ
+import re
 
 
 class Config:
+    # Articles repository
+    def __init__(self):
+        # Target the raw text files in case a github repository is provided.
+        self.ARTICLES_REPO = environ.get("articles_repo")
+
+        is_github = re.match(
+                      r".*github.com\/(\w+)\/(\w+)($|\.git$|\/tree/(\w+)$)",
+                      self.ARTICLES_REPO)
+        if is_github:
+            repo_details = is_github.groups()
+            # user, repository name, branch, respectively
+            self.ARTICLES_REPO = r"https://raw.githubusercontent.com/" +\
+                repo_details[0] + "/" + repo_details[1] +\
+                "/blob/" + repo_details[3]
 
     # General
-    SECRET_KEY = environ.get('SECRET_KEY')
+    secret_key = environ.get('secret_key')
 
     # Database
     SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
@@ -12,6 +27,3 @@ class Config:
         environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
     ARTICLES_PER_PAGE = 3
 
-    # Articles repository
-    # Should replace ordinary github filepaths with their raw counterparts
-    ARTICLES_REPO = environ.get("ARTICLES_REPO")
