8d350c11ec7fade380dbf7c50e4c54df4408ecb7
Odysseas Neslechanidis <odnes@tiptheiceberg.com>
2021-10-07 11:31:44 +0300

    Fix config.py

--- jackhammer/config.py
+++ jackhammer/config.py
@@ -3,6 +3,18 @@ import re
 
 
 class Config:
+    # Articles repository URL
+    ARTICLES_REPO = environ.get("ARTICLES_REPO")
+
+    # General
+    SECRET_KEY = environ.get('SECRET_KEY')
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = \
+        environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    ARTICLES_PER_PAGE = 3
+
     def __init__(self):
         # For ARTICLES_REPO, target the raw text files in case a github
         # repository is provided.
@@ -16,15 +28,4 @@ class Config:
                 repo_details[0] + "/" + repo_details[1] +\
                 "/blob/" + repo_details[3]
 
-    # Articles repository
-    ARTICLES_REPO = environ.get("ARTICLES_REPO")
-
-    # General
-    SECRET_KEY = environ.get('SECRET_KEY')
-
-    # Database
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = \
-        environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
-    ARTICLES_PER_PAGE = 3
 
