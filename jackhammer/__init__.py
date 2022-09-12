e8f3586013205a3c91f0799f82b57a1997083a97
Odysseas Neslechanidis <odnes@tiptheiceberg.com>
2022-09-12 14:34:53 +0300

    Clean up, rename project

--- jackhammer/__init__.py
+++
@@ -1,3 +0,0 @@
-# Repo at http://github.com/odnes/jackhammer.git
-
-__version__ = "0.1"
--- pneumatic/admin_app/__init__.py
+++ pneumatic/admin_app/__init__.py
@@ -4,7 +4,7 @@ from ..instantiate_db import db
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('jackhammer.config.default_config')
+    app.config.from_object('pneumatic.config.default_config')
 
     db.init_app(app)
 
